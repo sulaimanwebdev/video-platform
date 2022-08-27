be1fdf44ec0e4f61053cf85c0f35837402dc1ef6
Muhammad Sulaiman <trendingofyt@gmail.com>
2022-08-28 04:18:59 +0500

    Done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ const Header = ({account, active}) => {
     <>
      <div className="bg-white border-b-2 border-yellow-500 w-full">
        <div className="px-5 mx-auto max-w-[1350px] flex items-center justify-between h-[82px]">
-       <div><Link to='/'><div className='font-bold text-[29px]'>Logo</div></Link></div>
+       <div><Link to="/"><div className='font-bold text-[29px]'>Logo</div></Link></div>
        <div className="flex items-center bg-gray-100 rounded-full pr-3 gap-3">
        <div className="w-[55px] h-[55px] rounded-full flex items-center justify-center overflow-hidden"><img src="/images/avatar.png" alt="avatar" /></div>
         <div className=" rounded-md py-3">{active ? <span>{account.slice(0, 6)}...</span> : "..."}</div>
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = ({connect}) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+       if(localStorage?.getItem('isWalletConnected') === 'true'){
+        navigate('/home')
+  }
+       
+  }, [])
+
   return (
     <>
-    <div className="px-5 min-h-[calc(100vh-84px)] w-full flex items-center justify-center gap-8 flex-col  py-10">
-    <div className='font-bold text-[39px]'>Logo</div>
-    <button onClick={connect} className='rounded-md text-white bg-yellow-500 transition hover:bg-yellow-600 px-7 py-3 font-[600]'>Connect Wallet</button>
-    </div>
+    {
+      localStorage?.getItem('isWalletConnected') === 'true' ? "" : 
+      <div className="px-5 min-h-[calc(100vh-84px)] w-full flex items-center justify-center gap-8 flex-col  py-10">
+      <div className='font-bold text-[39px]'>Logo</div>
+      <button onClick={connect} className='rounded-md text-white bg-yellow-500 transition hover:bg-yellow-600 px-7 py-3 font-[600]'>Connect Wallet</button>
+      </div>
+    }
+    
     </>
   )
 }
