2232f9d5be70df6bd827c193851b1af633085deb
Muhammad Sulaiman <trendingofyt@gmail.com>
2022-08-27 22:04:42 +0500

    Allhamdulliah

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,10 @@ const Header = ({account, active}) => {
      <div className="bg-white border-b-2 border-yellow-500 w-full">
        <div className="px-5 mx-auto max-w-[1350px] flex items-center justify-between h-[82px]">
        <div><Link to='/'><div className='font-bold text-[29px]'>Logo</div></Link></div>
-       <div><button className='rounded-md text-white bg-yellow-500 transition hover:bg-yellow-600 px-7 py-3 font-[600]'>{active ? <span>Connected</span> : "Connect Wallet"}</button></div>
+       <div className="flex items-center bg-gray-100 rounded-full pr-3 gap-3">
+       <div className="w-[55px] h-[55px] rounded-full flex items-center justify-center overflow-hidden"><img src="/images/avatar.png" alt="avatar" /></div>
+        <div className=" rounded-md py-3">{active ? <span>{account.slice(0, 6)}...</span> : "..."}</div>
+       </div>
        </div>
      </div>
     </>
